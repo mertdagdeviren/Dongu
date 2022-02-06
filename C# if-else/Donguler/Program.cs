8bb31e3221cf574cc05ce6958ab823f44bfaade8
Mert Dağdeviren <dagdevirenmert0660@gmail.com>
2022-02-06 19:35:10 +0300

    Add files via upload

---
+++ C# if-else/Donguler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlamak için temel kurs";
+            string kurs3 = "Java";
+
+            //array-dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlamak için temel kurs" , "Java","python" };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For Bitti");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu-footer");
+        }
+    }
+}
